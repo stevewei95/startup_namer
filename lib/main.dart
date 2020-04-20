29de3c06672bc6bb0abc447f211f5039a53c3472
weixing13 <weixing@jd.com>
2020-04-20 16:53:14 +0800

    add package english_words

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: new Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center(
+          //child: const Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
